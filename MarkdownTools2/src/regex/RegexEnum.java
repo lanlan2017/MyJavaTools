93f03c38391f941e0de730b74632c32b78f1f84f
lanlan2017 <18251956727@163.com>
2019-12-19 16:26:25 +0800

    添加新的markdown功能

---
+++ MarkdownTools2/src/regex/RegexEnum.java
@@ -0,0 +1,88 @@
+package regex;
+
+/**
+ * 正则表达式枚举.
+ */
+public enum RegexEnum {
+    /**
+     * Markdown行内代码段.
+     */
+    // ToMDCodeInLine("`?([a-zA-Z/\\\\.@<$:'\"{]+:?(?!\\d)[a-zA-Z0-9\\\\/_*.+ ()<>=\\\\\",#'\\[\\]?-]+[a-zA-Z0-9>'\"}](?:\\)|\\(\\))?)`?"),
+    ToMDCodeInLine("`?((?:-(?! ))?[a-zA-Z<][a-zA-Z0-9 ():\\_.\\/\\[\\]<>,+=\"?-]*[a-zA-Z0-9);>/.\\*\\]])`?"),
+    /**
+     * 空行1.
+     */
+    blankLineRegex1("(?m)(?:\\n|\\r\\n)(?:^\\s*$)"),
+    /**
+     * 空行2.
+     */
+    blankLineRegex2("(?m)(?:^\\s*$(?:\\n|\\r\\n))"),
+    /**
+     * Markdown代码块.
+     */
+    markdownCodeBlockRegex("```\\w*\\n(.+\\n)+?```"),
+    /**
+     * 回车换行符.
+     */
+    CRLF("\\r?\\n"),
+    /**
+     * 要通过反射运行的方法,格式为:方法全称(参数标记)
+     */
+    methodToRunByReflection("(\\w+(?:\\.\\w+)+)\\(.*\\)"),
+    /**
+     * 花括号在方法定义所在行的末尾的getter和setter方法.
+     */
+    getterSetterDefaultStyle("public\\s+\\w+\\s+(?:set|get)\\w+\\((?:\\,?\\s*\\w+\\s+\\w+)*\\)\\s+\\{(?:(?:\\n|\\r\\n).*?)*?\\}"),
+    /**
+     * 花括号在方法定义的下一行的getter和setter方法
+     */
+    getterSetterLikeVC("public\\s+\\w+\\s+(?:set|get)\\w+\\((?:\\,?\\s*\\w+\\s+\\w+)*\\)(\\s+)\\{(?:(?:\\n|\\r\\n).*)*?\\1\\}"),
+    /**
+     * 从Kindle中复制文本时,kindle添加的引用标记文本.
+     */
+    KindleReferenceMark("(?m)\\n^$\\n.+Kindle.+"),
+    /**
+     * Markdown有序列表
+     */
+    MarkdownOrderListRegex("\\d."),
+    /**
+     * 多行文本的每一个行开头的位置
+     */
+    LineStart("(?m)^"),
+    /**
+     * 图片URL
+     */
+    ImgURL("https?://.*(?:\\.png|\\.jpg|\\.gif)"),
+    /**
+     * 匹配没有提示文本的图片:![](https://img-blog.csdnimg.cn/20191101155722848.png)
+     */
+    MdImgNoAlt("\\!\\[\\]\\((.+)\\)"),
+    /**
+     * 两个数字之间的位置,这个位置可以是点号
+     */
+    positionBetweenTwoNumbers("(?<=\\d)\\.?(?=\\d)"),
+    /**
+     * 匹配PDF中的书签.
+     */
+    PdfShuQianRegex("([0-9.]+)[ ]?([^0-9].+)"),
+    /**
+     * 中文后面的空格.
+     */
+    SpacesAfterChinese("((?<=[\\u4e00-\\u9fa5]))[ ]");
+
+    // 正则表达式字符串.
+    private String regex;
+
+    RegexEnum(String regex) {
+        this.regex = regex;
+    }
+
+    /**
+     * 获取枚举类对象中保存的正则表达式.
+     *
+     * @return 枚举类对象中保存的正则表达式字符串.
+     */
+    public String getRegex() {
+        return this.regex;
+    }
+}
---
+++ MarkdownTools2/src/tools/markdown/MarkdownTools.java
@@ -0,0 +1,116 @@
+package tools.markdown;
+
+import formatter.string.StringDeleter;
+import regex.RegexEnum;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author francis
+ * create at 2019/12/19-11:30
+ */
+public class MarkdownTools {
+
+    public String header1(String input) {
+        return "# " + input;
+    }
+
+    public String header2(String input) {
+        return "## " + input;
+    }
+
+    public String header3(String input) {
+        return "### " + input;
+    }
+
+    public String header4(String input) {
+        return "#### " + input;
+    }
+
+    public String header5(String input) {
+        return "##### " + input;
+    }
+
+    public String header6(String input) {
+        return "###### " + input;
+    }
+
+    public String headerAdd(String text) {
+        text = text.replaceAll("(#+)(.+)\\1", "#$1$2#$1");
+        return text;
+    }
+
+    public String headerMinus(String text) {
+        text = text.replaceAll("(#(#+))(.+)\\1", "$2$3$2");
+        return text;
+    }
+
+    public String bold(String input) {
+        return "**" + input + "**";
+    }
+
+    public String inlineCode(String input) {
+        return "`" + input + "`";
+    }
+
+    public String autoInlineCode(String input) {
+        return null;
+    }
+
+    public String hyperLinks(String input) {
+        Pattern pattern = Pattern.compile("(.+?)\\s{2,}(.+?)");
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            return "[" + matcher.group(1) + "](" + matcher.group(2) + ")";
+        }
+        return "[" + input + "](" + input + ")";
+    }
+
+    public String img(String input) {
+        Pattern legalInput = Pattern.compile(".+?([ ]{2,}).+?");
+        Matcher matcher = legalInput.matcher(input);
+        if (matcher.matches()) {
+            String delimiter = matcher.group(1);
+            final int index = input.indexOf(delimiter);
+            return "![" + input.substring(0, index) + "](" + input.substring(index + delimiter.length()) + ")";
+        }
+        return null;
+    }
+    public String unorderList(String text) {
+        text = new StringDeleter().deleteBlankLine(text);
+        // 开头不是字母数字或者中文的一律删除掉.
+        text = text.replaceAll("(?m)^[^a-zA-Z0-9\\u4e00-\\u9fa5][ ]+", "");
+        // 在每行开头添加上无序列表标记
+        text = text.replaceAll(RegexEnum.LineStart.getRegex(), "- ");
+        return text + "\n";
+    }
+
+    public String orderedList(String text) {
+        text = new StringDeleter().deleteBlankLine(text);
+        int i = 1;
+        String[] lines = text.split("\\n");
+        StringBuilder buf = new StringBuilder();
+        for (String string : lines) {
+            buf.append((i++) + ". " + string + "\n");
+        }
+        return buf.toString();
+    }
+    public String quote(String text) {
+        text = new StringDeleter().deleteBlankLine(text);
+        // 在多行文本的每一个行开头添加引用标记
+        text = text.replaceAll(RegexEnum.LineStart.getRegex(), "> ");
+        return text;
+    }
+    public String codeBlock(String language, String input) {
+        return "```" + language + "\n" + input + "\n```";
+    }
+
+    public String codeBlockJava(String input) {
+        return "```java\n" + input + "\n```";
+    }
+
+    public String codeBlockJavaScript(String input) {
+        return "```javascript\n" + input + "\n```";
+    }
+}
