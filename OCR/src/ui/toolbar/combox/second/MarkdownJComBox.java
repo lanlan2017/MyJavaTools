57943fac09df6d3476537f13a65cbbca6567906d
lanlan2017 <18251956727@163.com>
2020-06-27 00:27:30 +0800

    OCR模块,markdown功能添加新街加粗选项
    文字识别结束后对文字直接加粗

--- OCR/src/ui/toolbar/combox/second/MarkdownJComBox.java
+++ OCR/src/ui/toolbar/combox/second/MarkdownJComBox.java
@@ -1,6 +1,7 @@
 package ui.toolbar.combox.second;
 
 import formatter.Formatter;
+import formatter.markdown.MdBold;
 import formatter.markdown.MdInlineCodesOneLine;
 import formatter.markdown.multiline.MdInlineCodesMultiLine;
 import formatter.markdown.multiline.MdOrderList;
@@ -23,6 +24,7 @@ public class MarkdownJComBox {
     private static Formatter[] formatters = {
             new MdInlineCodesOneLine(),
             new MdInlineCodesMultiLine(),
+            new MdBold(),
             new MdUnorderList(),
             new MdOrderList(),
             new MdCbSql(),
@@ -33,7 +35,7 @@ public class MarkdownJComBox {
     private static Formatter defaultFormatter = formatters[0];
 
     private MarkdownJComBox() {
-        String[] markdownItems = {"单行代码", "多行代码", "无序列表","有序列表", "Java代码块", "SQL代码块","html代码块","JSP代码"};
+        String[] markdownItems = {"单行代码", "多行代码","加粗", "无序列表","有序列表", "Java代码块", "SQL代码块","html代码块","JSP代码"};
         comboBox = new JComboBox<>(markdownItems);
         comboBox.addItemListener(markdownE -> {
             // 如果是选中的话
@@ -52,30 +54,35 @@ public class MarkdownJComBox {
                         defaultFormatter = formatters[1];
                         break;
                     // 触发事件的选项
+                    case "加粗":
+                        System.out.println("格式化为:Markdown加粗");
+                        defaultFormatter = formatters[2];
+                        break;
+                    // 触发事件的选项
                     case "无序列表":
                         System.out.println("格式化为:Markdown无序列表");
-                        defaultFormatter = formatters[2];
+                        defaultFormatter = formatters[3];
                         break;
                     // 触发事件的选项
                     case "有序列表":
                         System.out.println("格式化为:Markdown无序列表");
-                        defaultFormatter = formatters[3];
+                        defaultFormatter = formatters[4];
                         break;
                     case "SQL代码块":
                         System.out.println("格式化为:markdown SQL代码块");
-                        defaultFormatter = formatters[4];
+                        defaultFormatter = formatters[5];
                         break;
                     case "Java代码块":
                         System.out.println("格式化为:markdown Java代码块");
-                        defaultFormatter = formatters[5];
+                        defaultFormatter = formatters[6];
                         break;
                     case "html代码块":
                         System.out.println("格式化为:markdown html代码块");
-                        defaultFormatter = formatters[6];
+                        defaultFormatter = formatters[7];
                         break;
                     case "JSP代码":
                         System.out.println("格式化为:markdown JSP代码");
-                        defaultFormatter = formatters[7];
+                        defaultFormatter = formatters[8];
                         break;
                 }
                 BaiduOcrRunable.setFormatter(defaultFormatter);
