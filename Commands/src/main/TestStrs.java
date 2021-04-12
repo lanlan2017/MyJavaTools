1655bdae12e8320c8868c14c215f7d23dd3a534b
lanlan2017 <18251956727@163.com>
2021-04-12 21:23:14 +0800

    html 2md命令 添加对无序列表，行内代码的支持
    支持对html无序列表转成Markdown
    支持html code转换成markdown

--- Commands/src/main/TestStrs.java
+++ Commands/src/main/TestStrs.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,99 +7,80 @@ import java.util.regex.Pattern;
  * 测试字符串处理
  */
 public class TestStrs {
-    //static Scanner scanner = new Scanner(Test.class.getResourceAsStream("/tools/html/toMdTable.html"));
     public static void main(String[] args) {
-        String str = "<div class=\"post-content\"><div>  <span>选<strong>B、C</strong></span> </div> <div>  <span>cat</span>：由第一行开始显示文件所有内容； </div> <div>  <span>tac</span>：从最后一行开始显示文件的所有内容，注意 tac 与cat 写法正好相反； </div> <div>  <span>more</span>：一页一页的显示文件内容，只能向后翻页； </div> <div>  <span>less</span>：也是一页一页显示文件内容，但是可以通过键盘上的【pagedown】，【pageup】控制向后，向前翻页； </div> <div>  <span>head</span>：显示一个文件的前几行； </div> <div>  <span>tail</span>：显示一个文件的后几行； </div></div>";
-        //System.out.println(htmlOneLineTable2MdTable(str));
-        System.out.println(toMd(str));
+        String input = "<details><summary>显示答案/隐藏答案</summary>正确答案: C</details>\n" +
+                "\n" +
+                "<ul> <li>esc，在vi的插入模式或者底行命令模式下进入命令模式 </li><li>exit，一般用于退出会话式程序或者交互式命令行，如ssh远程连接，切换 shell（Linux中） </li><li>q，用来退出 man 手册查看命令或者配置文件的状态，或者more、less等命令查看文件的状态 </li><li>quit，一般用于图形界面的退出 </li></ul> \n" +
+                "以上的exit和quit仅是个人总结，非权威，python中的exit()和quit()都可以用来退出python的交互式shell\n";
+        System.out.println(htmlUl2MdUl(input));
+
+
+        //appendReplacementTest();
     }
 
-    /**
-     * 将单行的HTML表格代码串，转换成Markdown表格.
-     *
-     * @param str HTML表格代码串，整个表格的代码都压缩在一行之中。
-     * @return Markdown表格
-     */
-    private static String htmlOneLineTable2MdTable(String str) {
-        //删除td标签之间的加粗标签
-        str = str.replaceAll("</?strong>", "**");
-        //散开标签
-        str = str.replaceAll("\\>(?:[ ])?\\<", ">\n<");
-        //System.out.println(str);
-        return htmlMultiLineTable2MdTable(str);
-    }
+    ///**
+    // * html 无序列表转markdown无序列表
+    // *
+    // * @param str 包含html无序列表的文本
+    // * @return 包含Markdown无序列表的文本
+    // */
+    //private static String htmlUl2MdUl(String str) {
+    //    Matcher htmlUlM = Pattern.compile("\\<ul\\>(.+?)\\<\\/ul\\>").matcher(str);
+    //    Pattern htmlLiP = Pattern.compile("\\<li\\>(.+?)\\<\\/li\\>");
+    //    StringBuilder sb = new StringBuilder();
+    //    if (htmlUlM.find()) {
+    //        String htmlUlLis = htmlUlM.group(1);
+    //        //System.out.println(htmlUlLis);
+    //        Matcher htmlLiM = htmlLiP.matcher(htmlUlLis);
+    //        while (htmlLiM.find()) {
+    //            //System.out.print("- "+htmlLiM.group(1)+"\n");
+    //            sb.append("- ").append(htmlLiM.group(1)).append("\n");
+    //            //htmlLiM.appendReplacement()
+    //        }
+    //        //System.out.println(sb.toString());
+    //    }
+    //    return sb.toString();
+    //}
 
     /**
-     * 把多行的HTML Table代码转换成Markdown表格。
+     * 替换HTML无序列表代码为Markdown无序列表
      *
-     * @param str HTML Table代码，这些代码每个标签都占据一行。
-     * @return Markdown表格
+     * @param str 包含html无序列表的文本
+     * @return 包含Markdown无序列表的文本
      */
-    public static String htmlMultiLineTable2MdTable(String str) {
-        //Scanner scanner = new Scanner(Test.class.getResourceAsStream("/tools/html/toMdTable.html"));
-        Scanner scanner = new Scanner(str);
-        String line;
-        boolean isout = false;
-        final Pattern thPattern = Pattern.compile("\\<(?:th|td)\\>(.+)\\<\\/(?:th|td)\\>");
-        StringBuilder sb = new StringBuilder();
-        int numberOfColumns = 0;
-        boolean isFirstTr = true;
-        while (scanner.hasNextLine()) {
-            line = scanner.nextLine();
-            //System.out.println(line);
-            if ("<tr>".equals(line)) {
-                isout = true;
-                continue;
-            }
-            if (isout) {
-                //System.out.println(line);
-                Matcher thMatcher = thPattern.matcher(line);
-                if (thMatcher.matches()) {
-                    //System.out.print("|" + thMatcher.group(1));
-                    sb.append("|").append(thMatcher.group(1));
-                    if (isFirstTr) {
-                        //计算列数
-                        numberOfColumns++;
-                    }
-                }
-            }
-            if ("</tr>".equals(line)) {
-                isout = false;
-                //System.out.print("|\n");
-                sb.append("|\n");
-                // 打印表格对齐方式
-                if (isFirstTr) {
-                    //System.out.println(numberOfColumns);
-                    for (int i = 0; i < numberOfColumns; i++) {
-                        //System.out.print("|:---");
-                        sb.append("|:---");
-                    }
-                    //System.out.print("|\n");
-                    sb.append("|\n");
-                    isFirstTr = false;
-                }
-                //continue;
-            }
+    private static String htmlUl2MdUl(String str) {
+        Matcher htmlUlM = Pattern.compile("\\<ul\\>(.+?)\\<\\/ul\\>").matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (htmlUlM.find()) {
+            htmlUlM.appendReplacement(sb, htmlUlLi2MdUlLi(htmlUlM.group(1)));
         }
+        htmlUlM.appendTail(sb);
         return sb.toString();
     }
 
     /**
-     * 移除html标签
-     *
-     * @return
+     * Html 无序列表代码转成Markdown无序列表。
+     * @param htmlUlCode html无序列表代码。
+     * @return Markdown无序列表代码。
      */
-    public static String toMd(String str) {
-        // 替换换行符
-        str = str.replaceAll("(?:<br>)+", "\n");
-        // 替换加粗标签
-        str = str.replaceAll("</?strong>", "**");
-        // 删除div,span标签
-        str = str.replaceAll("</?(?:div|span)(?: .*?)?>", "");
-        // 删除多余空格
-        str = str.replaceAll("[ ]+", " ");
-        // 删除多余的空格转义字符
-        str = str.replaceAll("(?:&nbsp;)+", " ");
-        return str;
+    private static String htmlUlLi2MdUlLi(String htmlUlCode){
+        Matcher m = Pattern.compile("\\<li\\>(.+?)\\<\\/li\\>").matcher(htmlUlCode);
+        StringBuilder sb=new StringBuilder(htmlUlCode.length());
+        while (m.find()){
+            sb.append("- ").append(m.group(1)).append("\n");
+        }
+
+        return sb.toString();
     }
+    private static void appendReplacementTest() {
+        Pattern p = Pattern.compile("cat");
+        Matcher m = p.matcher("one cat two cats in the yard");
+        StringBuffer sb = new StringBuffer();
+        while (m.find()) {
+            m.appendReplacement(sb, "dog");
+        }
+        m.appendTail(sb);
+        System.out.println(sb.toString());
+    }
+
 }
