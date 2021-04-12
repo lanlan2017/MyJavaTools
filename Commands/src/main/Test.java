6d740773d4f3ca416fc4c0cb40566424a4983e73
lanlan2017 <18251956727@163.com>
2021-04-12 22:35:55 +0800

    更新代码

--- Commands/src/main/Test.java
+++ Commands/src/main/Test.java
@@ -17,7 +17,6 @@ public class Test {
         //ConfigTools.getInstance().forward("h","clean");
         //ConfigTools.getInstance().forward("s","fqcn");
         //ConfigTools.getInstance().forward("m","nu");
-        //ConfigTools.getInstance().forward("s","cc","c");
         //ConfigTools.getInstance().forward("s");
         //ConfigTools.getInstance().forward("h","pre");
         //ConfigTools.getInstance().forward("j","f","c");
