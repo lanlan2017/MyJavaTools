c3e1f265fe69c1149f662772e81d70f1617f4646
lanlan2017 <18251956727@163.com>
2021-08-23 11:12:02 +0800

    添加功能：拖拽标签可以移动窗体

--- UITool/src/com/blue/ui/MainFrom.java
+++ UITool/src/com/blue/ui/MainFrom.java
@@ -2,14 +2,48 @@ package com.blue.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class MainFrom {
 
-    private JLabel lable;
     private JPanel panel;
+    private JLabel lable;
+    private JButton exitButton;
+    Point mousePressedPoint = new Point();
+
+    public MainFrom() {
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        lable.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                // 获取顶层的JFrame容器
+                JFrame frame = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class, panel);
+                // 获取窗体的位置
+                Point point = frame.getLocation();
+                // 设置窗体的新位置
+                frame.setLocation(point.x + e.getX() - (int) mousePressedPoint.getX(), point.y + e.getY() - (int) mousePressedPoint.getY());
+
+            }
+        });
+
+        lable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // 获取鼠标按压的位置
+                mousePressedPoint.x = e.getX();
+                mousePressedPoint.y = e.getY();
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("MainFrom");
+
         frame.setContentPane(new MainFrom().panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // frame.setType(Window.Type.NORMAL);
