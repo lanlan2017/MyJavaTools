5d326dadfd144625ae07a78b51e4dc68c64e5c38
lanlan2017 <18251956727@163.com>
2021-08-24 15:20:13 +0800

    更新CommandsUI模块，加入系统托盘功能
    虽然程序设置了永久至于顶层，但是有时候还是容易跑到其他层上去，又因为没有任务栏。这样就不容易找到程序，添加系统托盘则可以避免这种找不到程序的情况。
    就算程序的主界面被其他界面遮挡，找不到了，也可以点击系统托盘中的按钮来显示主界面。

--- CommandsUI/src/com/blue/ui/MainFrom.java
+++ CommandsUI/src/com/blue/ui/MainFrom.java
@@ -6,6 +6,7 @@ import tools.config.ConfigTools;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.net.URL;
 import java.util.Scanner;
 
 public class MainFrom {
@@ -19,7 +20,10 @@ public class MainFrom {
     private JScrollPane scrollPane;
 
     public MainFrom(JFrame frame) {
+        // 记下Frame
         this.frame = frame;
+        // 初始化系统托盘
+        initSystemTray();
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -223,6 +227,59 @@ public class MainFrom {
         // 整个窗体最小显示
         frame.pack();
     }
+    /**
+     * 初始化系统托盘
+     */
+    private void initSystemTray() {
+        // 判断系统是否支持托盘图标
+        if (SystemTray.isSupported()) {
+            // 如果系统支持系统托盘，那么不显示任务栏
+            frame.setType(Window.Type.UTILITY);
+
+            // // 获取托盘图标,图片请放在 当前包 下
+            // URL resource = this.getClass().getResource("/com/blue/ico/草莓_16.png");
+            URL resource = this.getClass().getResource("/com/blue/ico/工具_16.png");
+            // 创建图标
+            ImageIcon icon = new ImageIcon(resource);
+            // 创建弹出式菜单
+            PopupMenu pop = new PopupMenu();
+            // 创建 显示菜单项
+            MenuItem displayJFrameItem = new MenuItem("Show Frame");
+            // 给 显示窗体菜单项 添加事件处理程序
+            displayJFrameItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    frame.setVisible(true);
+                }
+            });
+            // 显示菜单项 添加到 弹出式菜单中
+            pop.add(displayJFrameItem);
+
+            // 创建 退出菜单项
+            MenuItem exitItem = new MenuItem("exit");
+            // 给 退出菜单项 添加事件监听器，单击时退出系统
+            exitItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.exit(0);
+                }
+            });
+            // 添加 退出菜单项 到弹出框中
+            pop.add(exitItem);
+
+            // 创建托盘图标程序
+            TrayIcon tray = new TrayIcon(icon.getImage(), "CommandsUI", pop);
+            // 获得系统托盘对象
+            SystemTray systemTray = SystemTray.getSystemTray();
+            try {
+                // 将托盘图标添加到系统托盘中
+                systemTray.add(tray);
+            } catch (AWTException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
 
     /**
      * 设置窗口初始界面
@@ -250,7 +307,7 @@ public class MainFrom {
         // frame.setType(Window.Type.POPUP);
 
         // 不显示任务栏
-        frame.setType(Window.Type.UTILITY);
+        // frame.setType(Window.Type.UTILITY);
         // 不显示标题栏，最小化，关闭按钮
         frame.setUndecorated(true);
         // 永远置顶
@@ -265,6 +322,8 @@ public class MainFrom {
         FlatLightLaf.setup();
         // 给所有的组件都使用该主题
         SwingUtilities.updateComponentTreeUI(frame);
+        // // 初始化系统托盘程序
+        // mainFrom.initSystemTray();
         // 最合适的方式显示
         frame.pack();
         // 显示窗体
