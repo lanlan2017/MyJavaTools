40284ddd67a8cb64825b5cee413161141cf9a8e2
lanlan2017 <18251956727@163.com>
2021-09-12 00:23:14 +0800

    build: 重新打包可执行程序

--- Tools/src/tools/string/PrintStr.java
+++ Tools/src/tools/string/PrintStr.java
@@ -2,9 +2,13 @@ package tools.string;
 
 public class PrintStr {
 
+    private static int count = 0;
+
     public static void printStr(String str) {
-        System.out.println("=============================================");
+
+        System.out.println("==================== start_" + count + " ====================");
         System.out.println(str);
-        System.out.println("=============================================");
+        System.out.println("===================== end_" + count + " =====================");
+        count++;
     }
 }
