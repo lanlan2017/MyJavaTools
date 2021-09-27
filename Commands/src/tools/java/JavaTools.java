6aa232365fb0bbb5cacb22a8f765aa7ac65c5ee2
lanlan2017 <18251956727@163.com>
2021-09-27 21:14:27 +0800

    fix: CommandsUI，`j format pdf`命令单行注释中有数字或英文字母时 无法正确格式化的问题

--- Commands/src/tools/java/JavaTools.java
+++ Commands/src/tools/java/JavaTools.java
@@ -87,7 +87,7 @@ public class JavaTools {
 
     public String formatFromPDF(String codeInOneLine) {
         // 替换中文单行注释
-        String regex = " ?(//[\\u4e00-\\u9fa5–—‘’“”…、。〈〉《》「」『』【】〔〕！（），．：；？]+) ";
+        String regex = " ?(//[a-zA-Z0-9\\u4e00-\\u9fa5–—‘’“”…、。〈〉《》「」『』【】〔〕！（），．：；？]+) ";
         // codeInOneLine = codeInOneLine.replaceAll(regex, "__LF__$1__LF__");
         codeInOneLine = codeInOneLine.replaceAll(regex, "$1\n");
         // 格式化java代码
