f6d7febdc148d2c84e3794ef2b43253997173b67
lanlan2017 <18251956727@163.com>
2021-09-29 22:29:56 +0800

    feat: Commands模块，`s format pdf`命令支持格式化无序列表

--- Commands/src/tools/string/formatter/PdfFormatter.java
+++ Commands/src/tools/string/formatter/PdfFormatter.java
@@ -11,22 +11,27 @@ import java.util.regex.Pattern;
 
 public class PdfFormatter {
     public String format(String sysClipboardText) {
-        String reuslt = "";
-        if (Pattern.compile("^[a-z]+ .+").matcher(sysClipboardText).find()) {
+        String reuslt = sysClipboardText;
+        if (Pattern.compile("^[a-z]+ .+").matcher(reuslt).find()) {
             PrintStr.printStr("是代码耶");
-            reuslt = new JavaTools().formatFromPDF(sysClipboardText);
+            reuslt = new JavaTools().formatFromPDF(reuslt);
             reuslt = new MarkdownTools().codeBlockJava(reuslt);
         }
         // 如果是多行的话
-        else if (Pattern.compile("^[\u4e00-\u9fa5]+.+").matcher(sysClipboardText).find()) {
+        else if (Pattern.compile("^[\u4e00-\u9fa5]+.+").matcher(reuslt).find()) {
             PrintStr.printStr("是中文耶");
-            if (sysClipboardText.matches("图\\d+-\\d+ .+")) {
+            if (reuslt.matches("图\\d+-\\d+ .+")) {
                 PrintStr.printStr("是图片提示");
                 reuslt = ConfigTools.getInstance().forward("html center".split(" "));
             } else {
-                reuslt = new StringDeleter().deleteSpaces(sysClipboardText);
+                // 删除中文之间的空白符
+                reuslt = new StringDeleter().deleteSpaces(reuslt);
+                // 删除换行符
                 reuslt = new StringDeleter().deleteCRLF(reuslt);
             }
+        }
+        if (reuslt.contains("● ")) {
+            reuslt = reuslt.replaceAll("● ", "- ");
         }
         return reuslt;
     }
