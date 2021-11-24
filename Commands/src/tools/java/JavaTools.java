6678d8f172ef4e701a0dbd844ae5ead618af70e9
lanlan2017 <18251956727@163.com>
2021-11-24 20:23:27 +0800

    fix: Commands模块，修复j format pdf命令没有正确格式化javaDoc的问题。
    调整方法的调用顺序,先格式化代码，再格式化javadoc注释。

--- Commands/src/tools/java/JavaTools.java
+++ Commands/src/tools/java/JavaTools.java
@@ -88,45 +88,24 @@ public class JavaTools {
     public String formatFromPDF(String codeInOneLine) {
         // 替换中文单行注释
         // String regex = " ?(//[a-zA-Z0-9\\u4e00-\\u9fa5–—‘’“”…、。〈〉《》「」『』【】〔〕！（），．：；？～]+) ";
-        // 替换单行注释
+        // PrintStr.printStr(codeInOneLine);
+        // 在单行注释后加上换行符
         String regex = " ?(//.+?) ";
         // codeInOneLine = codeInOneLine.replaceAll(regex, "__LF__$1__LF__");
         codeInOneLine = codeInOneLine.replaceAll(regex, "$1\n");
+        // PrintStr.printStr(codeInOneLine);
         // 文档注释单独一行显示
-        codeInOneLine = codeInOneLine.replaceAll(" ?(/\\*\\*.+?\\*/) ", "$1\n");
+        codeInOneLine = codeInOneLine.replaceAll(" ?(/\\*\\*.+?\\*/)(?: |\\n)", "$1\n");
+        // PrintStr.printStr(codeInOneLine);
         // 注解单独放在一行
         codeInOneLine = codeInOneLine.replaceAll("(?<!\\* )(@[a-zA-Z]+) ", "$1\n");
         // PrintStr.printStr(codeInOneLine);
         // 格式化java代码
-        codeInOneLine = formatAllJavaDoc(codeInOneLine);
-        // 恢复javaDoc
         codeInOneLine = JavaFormatter.formatJavaCodeInOneLine(codeInOneLine);
+        // 恢复javaDoc
+        // codeInOneLine = formatAllJavaDoc(codeInOneLine);
+        // PrintStr.printStr(codeInOneLine);
 
-        return codeInOneLine;
-    }
-
-    /**
-     * 格式java代码中的所有的JavaDoc
-     * @param codeInOneLine 包含杂乱的JavaDoc的java代码
-     * @return 恢复JavaDoc后的java代码。
-     */
-    private String formatAllJavaDoc(String codeInOneLine) {
-        Matcher matcher = Pattern.compile("(?m)^/\\*\\*\\*.+\\*/$").matcher(codeInOneLine);
-        if(matcher.find()){
-            // PrintStr.printStr("哈哈");
-            // if条件已经匹配过一次了，重置匹配器，从头开始开始匹配。
-            matcher.reset();
-            int length = codeInOneLine.length();
-            StringBuffer sb = new StringBuffer(length + length >> 2);
-            // 如果存在没有格式化的JavaDoc
-            while (matcher.find()) {
-                String javaDoc = matcher.group(0);
-                matcher.appendReplacement(sb, formatJavaDocStr(javaDoc));
-            }
-            matcher.appendTail(sb);
-            // 使用格式化JavaDoc后的代码作为处理结果
-            codeInOneLine =sb.toString();
-        }
         return codeInOneLine;
     }
 
@@ -198,35 +177,5 @@ public class JavaTools {
         methodCode = new StringDeleter().deleteCRLF(methodCode);
         methodCode = methodCode.replaceAll("\\{.+\\}", "{...}");
         return methodCode;
-    }
-
-    public static void main(String[] args) {
-        // String str="/*** 被动使用类字段演示三： * 常量在编译阶段会存入调用类的常量池中，本质上没有直接引用到定义常量的类，因此不会触发定义常量的 类的初始化 **/";
-        // String str = "/*** 非主动使用类字段演示 **/";
-        String str = "/*** 类加载器与instanceof关键字演示 ** @author zzm */";
-        if (str.matches("^/\\*\\*\\*.+\\*/$")) {
-            str = formatJavaDocStr(str);
-        }
-        System.out.println(str);
-    }
-
-    /**
-     * 格式化JavaDoc
-     *
-     * @param javaDocStr 错误的写在一行中的JavaDoc（从PDF中复制来的JavaDoc）
-     * @return 修复后的javaDoc
-     */
-    private static String formatJavaDocStr(String javaDocStr) {
-        // 替换JavaDoc的开始标记
-        javaDocStr = javaDocStr.replaceFirst("^/\\*\\*", "__JavaDoc_Start__");
-        // 替换JavaDoc的开始结束
-        javaDocStr = javaDocStr.replaceAll("\\*\\*/$", "__JavaDoc_End__");
-        // 分行
-        javaDocStr = javaDocStr.replaceAll("\\* ?", "\n $0");
-        // 恢复JavaDoc的开始标记
-        javaDocStr = javaDocStr.replace("__JavaDoc_Start__", "/**");
-        // 恢复JavaDoc的结束标记
-        javaDocStr = javaDocStr.replace("__JavaDoc_End__", "\n */");
-        return javaDocStr;
     }
 }
--- Commands/src/tools/java/formatter/JavaFormatter.java
+++ Commands/src/tools/java/formatter/JavaFormatter.java
@@ -4,11 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JavaFormatter {
     public static void main(String[] args) {
-        String input = "for (int i = 0; i < codeInOneLine.length(); i++) {char c = codeInOneLine.charAt(i);if (c == '{') {stack.push('{');} else if (c == '}') {stack.pop();}}";
-        System.out.println(formatJavaCodeInOneLine(input));
+        // String input = "for (int i = 0; i < codeInOneLine.length(); i++) {char c = codeInOneLine.charAt(i);if (c == '{') {stack.push('{');} else if (c == '}') {stack.pop();}}";
+        // System.out.println(formatJavaCodeInOneLine(input));
+
+        // String str="/*** 被动使用类字段演示三： * 常量在编译阶段会存入调用类的常量池中，本质上没有直接引用到定义常量的类，因此不会触发定义常量的 类的初始化 **/";
+        // String str = "/*** 非主动使用类字段演示 **/";
+        String str = "/*** 类加载器与instanceof关键字演示 ** @author zzm */";
+        if (str.matches("^/\\*\\*\\*.+\\*/$")) {
+            str = formatJavaDocStr(str);
+        }
+        System.out.println(str);
     }
 
     public static String formatJavaCodeInOneLine(String input) {
@@ -22,8 +32,11 @@ public class JavaFormatter {
         String javaCode = addTabs(input2);
         //匿名内部类有大括号后面的分号放在一行
         javaCode = javaCode.replaceAll("(?m)^([ ]+)\\}$\\n\\1;", "$1};");
+        // 恢复javaDoc
+        javaCode = formatAllJavaDoc(javaCode);
         return javaCode;
     }
+
 
     /**
      * 在多行代码前面加上制表符
@@ -109,4 +122,52 @@ public class JavaFormatter {
         }
         return tabSB.toString();
     }
+
+    /**
+     * 格式java代码中的所有的JavaDoc
+     *
+     * @param codeInOneLine 包含杂乱的JavaDoc的java代码
+     * @return 恢复JavaDoc后的java代码。
+     */
+    private static String formatAllJavaDoc(String codeInOneLine) {
+        Matcher matcher = Pattern.compile("(?m)^/\\*\\*\\*.+\\*/$").matcher(codeInOneLine);
+        // Matcher matcher = Pattern.compile("/\\*\\*\\*.+\\*/").matcher(codeInOneLine);
+        if (matcher.find()) {
+            // PrintStr.printStr("哈哈");
+            // if条件已经匹配过一次了，重置匹配器，从头开始开始匹配。
+            matcher.reset();
+            int length = codeInOneLine.length();
+            StringBuffer sb = new StringBuffer(length + length >> 2);
+            // 如果存在没有格式化的JavaDoc
+            while (matcher.find()) {
+                String javaDoc = matcher.group(0);
+                matcher.appendReplacement(sb, formatJavaDocStr(javaDoc));
+            }
+            matcher.appendTail(sb);
+            // 使用格式化JavaDoc后的代码作为处理结果
+            codeInOneLine = sb.toString();
+        }
+        return codeInOneLine;
+    }
+
+    /**
+     * 格式化JavaDoc
+     *
+     * @param javaDocStr 错误的写在一行中的JavaDoc（从PDF中复制来的JavaDoc）
+     * @return 修复后的javaDoc
+     */
+    private static String formatJavaDocStr(String javaDocStr) {
+        // 替换JavaDoc的开始标记
+        javaDocStr = javaDocStr.replaceFirst("^/\\*\\*", "__JavaDoc_Start__");
+        // 替换JavaDoc的开始结束
+        javaDocStr = javaDocStr.replaceAll("\\*\\*/$", "__JavaDoc_End__");
+        // 分行
+        javaDocStr = javaDocStr.replaceAll("\\* ?", "\n $0");
+        // 恢复JavaDoc的开始标记
+        javaDocStr = javaDocStr.replace("__JavaDoc_Start__", "/**");
+        // 恢复JavaDoc的结束标记
+        javaDocStr = javaDocStr.replace("__JavaDoc_End__", "\n */");
+        return javaDocStr;
+    }
 }
+
