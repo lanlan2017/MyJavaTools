0a3d50255ecb2706a2646de9d4dff44202a7236c
lanlan2017 <18251956727@163.com>
2021-11-27 00:36:25 +0800

    feat: Commands模块,添加`s format pdf mixed`命令。优化s format pdf命令
    s format pdf mixed命令用于格式化从pdf中按 段落、 java代码描述信息 、java代码；这样的顺序赋值下来的文本。
    修复s format pdf命令格式化无序列表时，没有删除掉中文词语之间多余的空格的问题

--- Commands/src/tools/string/formatter/PdfFormatter.java
+++ Commands/src/tools/string/formatter/PdfFormatter.java
@@ -5,53 +5,135 @@ import tools.copy.SystemClipboard;
 import tools.java.JavaTools;
 import tools.markdown.MarkdownTools;
 import tools.reflect.method.ObjectMap;
-import tools.string.PrintStr;
 import tools.string.StringDeleter;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PdfFormatter {
+    /**
+     * 格式化,段落，java代码描述，java代码，三段信息的混合信息。
+     * @param
+     * @return
+     */
+    public String formatPdjMixed(String mixedPdfText) {
+        Matcher matcher = Pattern.compile(" ?(代码清单\\d+-\\d+ .+?) ").matcher(mixedPdfText);
+        if (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            // 取出段落
+            String paragraphText = mixedPdfText.substring(0, start);
+
+            // 格式化段落
+            paragraphText = formatParagraph(paragraphText);
+            System.out.println("段落:" + paragraphText);
+
+            // 取出代码清单信息
+            // String centerText = mixedPdfText.substring(start, end);
+            // 取出java代码的描述信息
+
+            String javaCodeDescription = matcher.group(1);
+            System.out.println("代码描述信息:" + javaCodeDescription);
+            // 格式化代码清单描述信息
+            javaCodeDescription = formatCenter(javaCodeDescription);
+
+            // 取出java代码
+            String javaCode = mixedPdfText.substring(end);
+            System.out.println("java代码:" + javaCode);
+            // 格式化java代码
+            javaCode = formatJavaCode(javaCode);
+
+            mixedPdfText = paragraphText + "\n\n" + javaCodeDescription + "\n\n" + javaCode + "\n";
+        }
+        return mixedPdfText;
+    }
+
     public String format(String sysClipboardText) {
         String reuslt = sysClipboardText;
         // System.out.println("进入方法了");
         // 如果是java代码的话
         if (isJavaCode(reuslt)) {
-            // System.out.println("是java代码耶");
-            // 从对象池中的获取JavaTools对象，免得重复创建对象。
-            JavaTools javaTools = ObjectMap.getObjectFromMap(JavaTools.class);
-            // 从对象池中的获取MarkdownTools对象，免得重复创建对象。
-            MarkdownTools markdownTools = ObjectMap.getObjectFromMap(MarkdownTools.class);
-            // 格式化java代码
-            reuslt = javaTools.formatFromPDF(reuslt);
-            // 生成markdown的java代码块
-            reuslt = markdownTools.codeBlockJava(reuslt);
-            // reuslt = "\n```java\n" + reuslt + "\n```\n";
+            reuslt = formatJavaCode(reuslt);
         }
         // 如果开头是中文的话,或者开头有一个英文单词，后面都是中文
         else if (Pattern.compile("^(?:[a-zA-Z]+)?[\u4e00-\u9fa5]+.+").matcher(reuslt).find()) {
             // System.out.println("是中文耶");
             // 如果是图片描述信息，如果是代码清单描述信息
             if (reuslt.matches("图\\d+-\\d+ .+") || reuslt.matches("代码清单\\d+-\\d+ .+")) {
-                // PrintStr.printStr("是图片提示");
-                // 居中对齐
-                return "<center>" + reuslt + "</center>";
+                reuslt = formatCenter(reuslt);
             } else {
-                // 从对象池中获取对象，免得重复创建对象。
-                StringDeleter stringDeleter = ObjectMap.getObjectFromMap(StringDeleter.class);
-                // 删除中文之间的空白符
-                reuslt = stringDeleter.deleteSpaces(reuslt);
-                // 删除换行符
-                reuslt = stringDeleter.deleteCRLF(reuslt);
+                // 格式化段落
+                reuslt = formatParagraph(reuslt);
             }
         }
         // 如果以小圆点开头的话（Office的无序列表标志）
         else if (reuslt.startsWith("·") || reuslt.startsWith("•") || reuslt.startsWith("●")) {
-            // System.out.println("是无序列表");
-            // 使用对象池中的对象，免得重复创建对象。
-            MarkdownTools markdownTools = ObjectMap.getObjectFromMap(MarkdownTools.class);
-            reuslt = markdownTools.unorderList(reuslt);
+            // 格式化无序列表
+            reuslt = formatUnorderedList(reuslt);
         }
         return reuslt;
+    }
+
+    /**
+     * 格式化java代码。
+     *
+     * @param javaCode
+     * @return
+     */
+    private String formatJavaCode(String javaCode) {
+        // System.out.println("是java代码耶");
+        // 从对象池中的获取JavaTools对象，免得重复创建对象。
+        JavaTools javaTools = ObjectMap.getObjectFromMap(JavaTools.class);
+        // 从对象池中的获取MarkdownTools对象，免得重复创建对象。
+        MarkdownTools markdownTools = ObjectMap.getObjectFromMap(MarkdownTools.class);
+        // 格式化java代码
+        javaCode = javaTools.formatFromPDF(javaCode);
+        // 生成markdown的java代码块
+        javaCode = markdownTools.codeBlockJava(javaCode);
+        return javaCode;
+    }
+
+    /**
+     * 格式化居中对齐文本
+     *
+     * @param toBeCenter
+     * @return html居中对齐标签
+     */
+    private String formatCenter(String toBeCenter) {
+        // 居中对齐
+        return "<center>" + toBeCenter + "</center>";
+    }
+
+    /**
+     * 格式化无序列表
+     *
+     * @param reuslt 无序列表内容
+     * @return markdown格式的无序列表
+     */
+    private String formatUnorderedList(String reuslt) {
+        StringDeleter stringDeleter=ObjectMap.getObjectFromMap(StringDeleter.class);
+        reuslt=stringDeleter.deleteSpaces(reuslt);
+        // System.out.println("是无序列表");
+        // 使用对象池中的对象，免得重复创建对象。
+        MarkdownTools markdownTools = ObjectMap.getObjectFromMap(MarkdownTools.class);
+        reuslt = markdownTools.unorderList(reuslt);
+        return reuslt;
+    }
+
+    /**
+     * 格式化段落
+     *
+     * @param paragraphText 段落文本
+     * @return 格式化后的段落文本。
+     */
+    private String formatParagraph(String paragraphText) {
+        // 从对象池中获取对象，免得重复创建对象。
+        StringDeleter stringDeleter = ObjectMap.getObjectFromMap(StringDeleter.class);
+        // 删除中文之间的空白符
+        paragraphText = stringDeleter.deleteSpaces(paragraphText);
+        // 删除换行符
+        paragraphText = stringDeleter.deleteCRLF(paragraphText);
+        return paragraphText;
     }
 
     /**
