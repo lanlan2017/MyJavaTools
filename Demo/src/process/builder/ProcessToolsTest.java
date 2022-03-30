5c68fcc09858fbc4b40b3a0f239c78597aefc546
lanlan2017 <18251956727@163.com>
2022-03-30 14:12:52 +0800

    refactor: 移动代码，ProcessToolsTest从Tools模块移动到Demo模块
    ProcessToolsTest并不是工具类，而是演示代码，不应该放在Tools模块中，应该放到Demo模块中。

--- Demo/src/process/builder/ProcessToolsTest.java
+++ Demo/src/process/builder/ProcessToolsTest.java
@@ -1,4 +1,4 @@
-package test.process;
+package process.builder;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,11 +12,15 @@ public class ProcessToolsTest {
     }
 
     private static void showHelp() {
+        // 创建命令列表
         ArrayList<String> commands = new ArrayList<>();
         commands.add("notepad.exe");
         commands.add("help.txt");
+
         try {
+            // 创建进程构造器
             ProcessBuilder runner = new ProcessBuilder(commands);
+            // 执行命令
             runner.start();
         } catch (IOException e) {
             e.printStackTrace();
