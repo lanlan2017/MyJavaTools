0831d0334832aa6cd925e3ffdc6b8b555a62dccf
lanlan2017 <18251956727@163.com>
2022-04-18 03:42:31 +0800

    refactor: 打包可执行jar,删除无用jar

--- runable/FM.bat
+++ runable/FM.bat
@@ -1,4 +1,6 @@
 @echo off
+@REM 修改当前cmd的编码为utf8
+@REM chcp 65001
 :: 进入当前盘符
 %~d0
 :: 进入当前文件所在目录
@@ -8,5 +10,6 @@ echo Current working directory:%cd%
 :: 执行java
 @REM echo %~dp0FM.jar
 @REM java -jar "%~dp0FM.jar" %1 %2 %3
-java -jar "%~dp0FM.jar" %1 %2 %3
+@REM java -Dfile.encoding=utf-8 -jar "%~dp0FM.jar" %1 %2 %3
+java -Dfile.encoding=utf-8 -jar "%~dp0Commands.jar" hexo %*
 @REM pause
