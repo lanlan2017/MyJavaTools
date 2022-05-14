c9fc63ab02842c6ae5e925243411a444e984cecd
lanlan2017 <18251956727@163.com>
2022-05-14 15:26:17 +0800

    refactor: 修改可执行程序mytree

--- runable/mytree.bat
+++ runable/mytree.bat
@@ -1,9 +1,1 @@
-@echo off
-:: 进入当前盘符
-%~d0
-:: 进入当前文件所在目录
-cd %~dp0
-:: 显示当前工作路径
-echo Current working directory:%cd%
-:: 执行java
-java -jar "%~dp0Tree.jar" %1 %2 %3
+java -jar "%~dp0Commands.jar" mytree %*
