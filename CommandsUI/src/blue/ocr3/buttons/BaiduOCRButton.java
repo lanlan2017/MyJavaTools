71f767708e78a5f0487f5914e606a0eaf43e3ce5
lanlan2017 <18251956727@163.com>
2022-05-15 17:26:01 +0800

    fix: CommandsUI模块，解决每次OCR的时候都要调整窗口大小的问题。
    第一次OCR的时候，因为接收OCR输出的文本域还没显示，所需需要调用frame.pack()动态调整大小。
    后面OCR的时候，就不需要调用frame.pack()了

--- CommandsUI/src/blue/ocr3/buttons/BaiduOCRButton.java
+++ CommandsUI/src/blue/ocr3/buttons/BaiduOCRButton.java
@@ -34,15 +34,24 @@ public class BaiduOCRButton extends ButtonKeyAction {
         // 显示ocr输出Pane
         JScrollPane ocrScrollPane = MainFrom.getInstance().getOcrScrollPane();
         ocrScrollPane.setVisible(true);
-        // 显示滚动面板的顶级Pane
-        // MainFrom.getInstance().getScrollPaneFather().setVisible(true);
-        // 显示分割面板
-        MainFrom.getInstance().getJSplitPane().setVisible(true);
+        // 获取OCR输出文本域所在的分割面板
+        JSplitPane jSplitPane = MainFrom.getInstance().getJSplitPane();
+        // 如果OCR输出文本域的顶级面板不可见的话，就显示，并且调整窗体大小
+        if(!jSplitPane.isVisible()){
+            // 显示OCR输出文本域的父面板
+            jSplitPane.setVisible(true);
+            MainFrom.getInstance().getFrame().pack();
+
+        }
+        // 如果分割面板左侧被隐藏的话，则显示分割面板
+        else if(jSplitPane.getDividerLocation()==0){
+            int last= jSplitPane.getLastDividerLocation();
+            jSplitPane.setDividerLocation(last);
+        }
 
         // 把文字识别的结果写入到输入文本域中
         inputTextArea.setText(ocrStr);
         // 调整窗体的大小
-        MainFrom.getInstance().getFrame().pack();
         // 将识别结果写到剪贴板中.
         SystemClipboard.setSysClipboardText(ocrStr);
     }
