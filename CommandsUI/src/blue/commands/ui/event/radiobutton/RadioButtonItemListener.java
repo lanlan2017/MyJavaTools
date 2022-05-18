9f6cfa70f416035ec7216303adba5b4848d6d074
lanlan2017 <18251956727@163.com>
2022-05-18 11:13:54 +0800

    feat: CommandsUI模块，更新ocr按钮事件处理函数
    关闭ocr面板时，隐藏分隔条左侧的面板
    开启ocr面板时，显示分隔条左侧的面板

--- CommandsUI/src/blue/commands/ui/event/radiobutton/RadioButtonItemListener.java
+++ CommandsUI/src/blue/commands/ui/event/radiobutton/RadioButtonItemListener.java
@@ -9,6 +9,7 @@ import java.awt.event.ItemListener;
 public class RadioButtonItemListener implements ItemListener {
     JFrame frame;
     JPanel ocrPanel;
+    private int last;
 
     public RadioButtonItemListener(JFrame frame, JPanel ocrPanel) {
         this.frame = frame;
@@ -17,6 +18,19 @@ public class RadioButtonItemListener implements ItemListener {
 
     @Override
     public void itemStateChanged(ItemEvent e) {
+        JSplitPane jSplitPane = MainFrom.getInstance().getJSplitPane();
+        // 如果OCR面板可见的话
+        if (ocrPanel.isVisible()) {
+            // 获取分隔条的位置
+            last = jSplitPane.getDividerLocation();
+            // 把分隔条的位置设置为0
+            // 隐藏分隔条左侧的面板
+            jSplitPane.setDividerLocation(0);
+        } else {
+            // 调整分隔条到原来的位置
+            // 显示分隔条左侧的面板
+            jSplitPane.setDividerLocation(last);
+        }
         // 显示或隐藏 OCR面板
         ocrPanel.setVisible(!ocrPanel.isVisible());
         // 以合适大小显示窗体
