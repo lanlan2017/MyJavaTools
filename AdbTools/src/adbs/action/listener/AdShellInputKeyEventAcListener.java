e95ad7c738f128d2351dbda3db9e9898368cddc6
lanlan2017 <18251956727@163.com>
2022-06-08 11:45:56 +0800

    fix: AdbTools模块，解决 部分按钮 长时间不释放焦点的问题

--- AdbTools/src/adbs/action/listener/AdShellInputKeyEventAcListener.java
+++ AdbTools/src/adbs/action/listener/AdShellInputKeyEventAcListener.java
@@ -1,8 +1,10 @@
 package adbs.action.listener;
 
+import adbs.buttons.JButtons;
 import adbs.cmd.AdbCommands;
 import adbs.test.DeviceRadioBtAcListener;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,6 +15,11 @@ public abstract class AdShellInputKeyEventAcListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // JButton button = (JButton) e.getSource();
+        // // 释放焦点
+        // button.setFocusPainted(false);
+        JButtons.setFocusPainted(e);
+
         String id = DeviceRadioBtAcListener.getId();
         // 调用子类的方法，设置键值
         setKey();
--- AdbTools/src/adbs/action/listener/OpenButtonListener.java
+++ AdbTools/src/adbs/action/listener/OpenButtonListener.java
@@ -1,6 +1,7 @@
 package adbs.action.listener;
 
 import adbs.action.runnable.OpenButtonRunnable;
+import adbs.buttons.JButtons;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -8,24 +9,14 @@ import java.awt.event.ActionListener;
 
 public class OpenButtonListener implements ActionListener {
     private OpenButtonRunnable target;
-    // private JFrame frame;
-    //
-    // public OpenButtonListener(JFrame frame) {
-    //     target = new OpenButtonRunnable();
-    //     this.frame = frame;
-    // }
-    public OpenButtonListener()
-    {
+
+    public OpenButtonListener() {
         target = new OpenButtonRunnable();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // if (e.getSource() instanceof JRadioButton) {
-        //     System.out.println("是单按钮");
-        //     JRadioButton select = (JRadioButton) e.getSource();
-        //     frame.setTitle(select.getText());
-        // }
+        JButtons.setFocusPainted(e);
         new Thread(target).start();
     }
 }
