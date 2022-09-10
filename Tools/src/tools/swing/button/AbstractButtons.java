d0b1d7a5a3470c7068bbaf62b332518c5e75f828
lanlan2017 <18251956727@163.com>
2022-09-10 21:02:22 +0800

    feat: Tools模块,添加新的工具方法

--- Tools/src/tools/swing/button/AbstractButtons.java
+++ Tools/src/tools/swing/button/AbstractButtons.java
@@ -57,4 +57,27 @@ public class AbstractButtons {
         }
         panel.repaint();
     }
+    /**
+     *
+     */
+    /**
+     *
+     * 设置JPanel中所有按钮的内边距
+     * @param panel 放置按钮的JPanel
+     * @param x 内边距的值
+     */
+    public static void setMarginInButtonJPanel(JPanel panel,int x) {
+        int count = panel.getComponentCount();
+        // int x = 3;
+        Insets insets = new Insets(x, x, x, x);
+        for (int i = 0; i < count; i++) {
+            Component component = panel.getComponent(i);
+            if (component instanceof AbstractButton) {
+                // System.out.println(component + "是按钮");
+                AbstractButton abstractButton = (AbstractButton) component;
+                abstractButton.setMargin(insets);
+            }
+        }
+        panel.repaint();
+    }
 }
