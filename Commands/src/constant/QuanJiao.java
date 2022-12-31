c053295595926334b73270f28befe3591caaae22
lanlan2017 <18251956727@163.com>
2023-01-01 00:24:54 +0800

    fix: Commands,修复版本后退引起的bug

---
+++ Commands/src/constant/QuanJiao.java
@@ -0,0 +1,12 @@
+package constant;
+
+public class QuanJiao {
+    /**
+     * 全角空格
+     */
+    public static String kongGe="　";
+    /**
+     * 中文全角横线
+     */
+    public static String HengXian="－";
+}
