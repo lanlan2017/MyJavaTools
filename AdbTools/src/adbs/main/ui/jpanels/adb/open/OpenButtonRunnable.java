054f511c8c4729dad151a1f45b0043c8c10a8b02
lanlan2017 <18251956727@163.com>
2023-08-10 11:24:52 +0800

    feat: Adbtools模块,打开scrcpy投屏时,关闭手机自动亮度,调整亮度为0

--- AdbTools/src/adbs/main/ui/jpanels/adb/open/OpenButtonRunnable.java
+++ AdbTools/src/adbs/main/ui/jpanels/adb/open/OpenButtonRunnable.java
@@ -1,7 +1,9 @@
 package adbs.main.ui.jpanels.adb.open;
 
+import adbs.cmd.AdbCommands;
 import adbs.cmd.CmdRun;
 import adbs.main.AdbTools;
+import adbs.tools.thread.ThreadSleep;
 
 import javax.swing.*;
 
@@ -22,25 +24,46 @@ public class OpenButtonRunnable implements Runnable {
     @Override
     public void run() {
         // 获取选择的id
-        // String id = AdbTools.device.getId();
-        // String simpleId = AdbTools.device.getSimpleId();
+        // String serial = AdbTools.device.getId();
+        // String name = AdbTools.device.getSimpleId();
 
         // 获取序列号
-        String id = AdbTools.getInstance().getDevice().getSerial();
-        String simpleId = AdbTools.getInstance().getDevice().getName();
+        String serial = AdbTools.getInstance().getDevice().getSerial();
+        String name = AdbTools.getInstance().getDevice().getName();
 
         // 获取宽度
-        String scrcpyWidth=widthTextField.getText();
+        String scrcpyWidth = widthTextField.getText();
         // 如果存在id
-        if (id != null && !"".equals(id)) {
+        if (serial != null && !"".equals(serial)) {
             System.out.println("scrcpy.exe 打开镜像");
+            // 关闭亮度自动调节
+            // turnOffAutoBrightness
+
+            // String turnOffAutoBrightness = "adb -s U8ENW18118023171 shell settings put system screen_brightness_mode 0";
+            String turnOffAutoBrightness = "adb -s " + serial + " shell settings put system screen_brightness_mode 0";
+            AdbCommands.runAbdCmd(turnOffAutoBrightness);
+            ThreadSleep.seconds(3);
+            // 亮度调为0
+            // 最小亮度
+            // miniBrightness
+            // String xxx="adb -s U8ENW18118023171 shell settings put system screen_brightness 0";
+            // String miniBrightness = "adb -s " + serial + " shell settings put system screen_brightness ";
+            // if ("jjqsqst4aim7f675".equals(serial)) {
+            //     miniBrightness += "2";
+            // } else {
+            //     miniBrightness += "0";
+            // }
+            String miniBrightness = "adb -s " + serial + " shell settings put system screen_brightness 0";
+            AdbCommands.runAbdCmd(miniBrightness);
+
+
             // 拼接命令
             String code;
 
-            if(scrcpyWidth!=null&&scrcpyWidth.matches("\\d+")){
-                code = "adbtools_open_scrcpy.bat " + id + " " + simpleId+ " " +scrcpyWidth;
-            }else {
-                code = "adbtools_open_scrcpy.bat " + id + " " + simpleId;
+            if (scrcpyWidth != null && scrcpyWidth.matches("\\d+")) {
+                code = "adbtools_open_scrcpy.bat " + serial + " " + name + " " + scrcpyWidth;
+            } else {
+                code = "adbtools_open_scrcpy.bat " + serial + " " + name;
             }
             System.out.println("调用另一个Jar:" + code);
             CmdRun.run(code);
