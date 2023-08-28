8fafde83af988e3b885236f7606c147781e0cedb
lanlan2017 <18251956727@163.com>
2023-08-28 17:51:27 +0800

    feat: Tools模块，更新PropertiesTools.java，添加store方法用来更新配置文件的内容。

--- Tools/src/tools/config/properties/PropertiesTools.java
+++ Tools/src/tools/config/properties/PropertiesTools.java
@@ -2,6 +2,7 @@ package tools.config.properties;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Iterator;
@@ -15,9 +16,12 @@ public class PropertiesTools {
 
     public static String withoutThisVlaue = "找不到这个value对应的key";
 
+    // FileOutputStream fileOutputStream;
+    private File file;
+
 
     public PropertiesTools(String path) {
-        File file = new File(path);
+        file = new File(path);
         // System.out.println("配置文件绝对路径= " + file.getAbsolutePath());
         load(file);
     }
@@ -30,6 +34,8 @@ public class PropertiesTools {
         if (file.exists()) {
             try {
                 properties.load(new FileInputStream(file));
+                // 新加入
+                // fileOutputStream = new FileOutputStream(file);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -50,21 +56,13 @@ public class PropertiesTools {
     //
     // }
 
-    public static void main(String[] args) {
-        // URL resource = PropertiesTools.class.getClassLoader().getResource("config/properties/Test.properties");
-        // String path = resource.getPath();
-        // String path = "G:\\dev2\\idea_workspace\\MyJavaTools\\AdbTools\\Pythons\\ui.properties";
-        String path = "G:\\dev2\\idea_workspace\\MyJavaTools\\AdbTools.properties";
-        // System.out.println("file path = " + path);
-        PropertiesTools propertiesTools = new PropertiesTools(path);
-        // test0(propertiesTools);
-        // test1(propertiesTools);
-        // test2(propertiesTools);
 
-        String toSearchFor = "9-";
-        System.out.println(propertiesTools.findKey(toSearchFor));
-    }
-
+    /**
+     * 根据value查找value
+     *
+     * @param value 要查找的value
+     * @return 如果找到对应的key则返回查找到的key，否则返回withoutThisVlaue
+     */
     public String findKey(String value) {
         // 获取所有键的Set
         Set<String> stringSet = keySet();
@@ -82,6 +80,83 @@ public class PropertiesTools {
         }
         return withoutThisVlaue;
     }
+
+
+    /**
+     * 返回包含所有key的Set<String>集合
+     *
+     * @return 包含配置文件所有key的Set<String>
+     */
+    private Set<String> keySet() {
+        return properties.stringPropertyNames();
+    }
+
+    /**
+     * 获取所有key的枚举
+     *
+     * @return 获取所有key的枚举
+     */
+    private Enumeration<?> getKeyEnumeration() {
+        return properties.propertyNames();
+    }
+
+    public void setProperty(String key, String value) {
+        properties.setProperty(key, value);
+    }
+
+    public void store() {
+        try {
+            // properties.store(fileOutputStream, "更新端口号");
+            // fileOutputStream.close();
+            // fileOutputStream=new FileOutputStream(file);
+            // fileOutputStream.flush();
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            properties.store(fileOutputStream, "更新端口号");
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        // URL resource = PropertiesTools.class.getClassLoader().getResource("config/properties/Test.properties");
+        // String path = resource.getPath();
+        // String path = "G:\\dev2\\idea_workspace\\MyJavaTools\\AdbTools\\Pythons\\ui.properties";
+        // testFindKey();
+        testStore();
+        testStore();
+        testStore();
+    }
+
+    private static void testStore() {
+        String path = "G:\\dev2\\idea_workspace\\MyJavaTools\\AdbTools_Port.properties";
+        PropertiesTools propertiesTools = new PropertiesTools(path);
+        String nextPort = propertiesTools.getProperty("NextPort");
+        System.out.println("nextPort = " + nextPort);
+
+        int port = Integer.parseInt(nextPort);
+        String nextPort1 = String.valueOf(port + 1);
+
+        propertiesTools.setProperty("NextPort", nextPort1);
+
+        // 保存到配置文件中
+        propertiesTools.store();
+    }
+
+
+    private static void testFindKey() {
+        String path = "G:\\dev2\\idea_workspace\\MyJavaTools\\AdbTools.properties";
+        // System.out.println("file path = " + path);
+        PropertiesTools propertiesTools = new PropertiesTools(path);
+        // test0(propertiesTools);
+        // test1(propertiesTools);
+        // test2(propertiesTools);
+
+        String toSearchFor = "9-";
+        System.out.println(propertiesTools.findKey(toSearchFor));
+    }
+
 
     private void test0() {
         String site = getProperty("site");
@@ -106,21 +181,4 @@ public class PropertiesTools {
         }
     }
 
-    /**
-     * 返回包含所有key的Set<String>集合
-     *
-     * @return 包含配置文件所有key的Set<String>
-     */
-    private Set<String> keySet() {
-        return properties.stringPropertyNames();
-    }
-
-    /**
-     * 获取所有key的枚举
-     *
-     * @return 获取所有key的枚举
-     */
-    private Enumeration<?> getKeyEnumeration() {
-        return properties.propertyNames();
-    }
 }
