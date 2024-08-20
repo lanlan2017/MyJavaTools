b08d6b5f82c741f42dd2911944a304d7392ce3c7
lanlan2017 <18251956727@163.com>
2024-08-20 17:19:49 +0800

    fix: 浏览任务的时间选择面板不能正确的增加和减小的问题

--- AdbTools/src/adbs/main/ui/jpanels/universal/listener/BrowseButtonActionListener.java
+++ AdbTools/src/adbs/main/ui/jpanels/universal/listener/BrowseButtonActionListener.java
@@ -3,6 +3,7 @@ package adbs.main.ui.jpanels.universal.listener;
 import adbs.main.ui.jframe.JFramePack;
 import adbs.main.ui.jpanels.adb.listener.ButtonFocusReleaseActionListener;
 import adbs.main.ui.jpanels.time.TimePanels;
+import adbs.main.ui.jpanels.time.listener.WaitValues;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -11,7 +12,7 @@ import java.awt.event.ActionEvent;
  * 设置浏览按钮的操作界面。
  */
 public class BrowseButtonActionListener extends ButtonFocusReleaseActionListener {
-    private TimePanels timePanels;
+    private final TimePanels timePanels;
 
     public BrowseButtonActionListener(TimePanels timePanels) {
         this.timePanels = timePanels;
@@ -19,32 +20,40 @@ public class BrowseButtonActionListener extends ButtonFocusReleaseActionListener
 
     @Override
     protected void actionEvent(ActionEvent e) {
-//        SwingUtilities.invokeLater(new Runnable() {
-//            @Override
-//            public void run() {
+        //        SwingUtilities.invokeLater(new Runnable() {
+        //            @Override
+        //            public void run() {
 
 
-                timePanels.getTimePanel().setVisible(true);
-                timePanels.getTimeLabel().setText("时间(s):");
-                // inputPanels.getTimeRadioPanel().setVisible(true);
-                timePanels.getInput1().setText(String.valueOf(5));
-                timePanels.getInput1().setColumns(4);
-                timePanels.getInput2().setVisible(false);
-                timePanels.getPlusBtn().setVisible(true);
-                timePanels.getMinusBtn().setVisible(true);
+        timePanels.getTimePanel().setVisible(true);
+        timePanels.getTimeLabel().setText("时间(s):");
+        // inputPanels.getTimeRadioPanel().setVisible(true);
+        JTextField input1 = timePanels.getInput1();
 
-                timePanels.getTaskCheckBox().setVisible(false);
-                JCheckBox stopCheckBox = timePanels.getStopCheckBox();
-                stopCheckBox.setVisible(false);
+//        int index = 5;
 
-                timePanels.getInputOkButton().setText("开始浏览");
-                // timePanels.getTimerJLabel().setVisible(false);
-                // 调整窗体到最佳大小
-                // JFramePack.onJComponentActionEvent(e);
-                 JFramePack.pack();
-//                AdbTools.getInstance().getFrame().pack();
-//            }
-//        });
+        int index = 0;
+        WaitValues.setIndex(index);
+        input1.setText(String.valueOf(WaitValues.values[index]));
+//        input1.setText(String.valueOf(index));
+
+        input1.setColumns(4);
+        timePanels.getInput2().setVisible(false);
+        timePanels.getPlusBtn().setVisible(true);
+        timePanels.getMinusBtn().setVisible(true);
+
+        timePanels.getTaskCheckBox().setVisible(false);
+        JCheckBox stopCheckBox = timePanels.getStopCheckBox();
+        stopCheckBox.setVisible(false);
+
+        timePanels.getInputOkButton().setText("开始浏览");
+        // timePanels.getTimerJLabel().setVisible(false);
+        // 调整窗体到最佳大小
+        // JFramePack.onJComponentActionEvent(e);
+        JFramePack.pack();
+        //                AdbTools.getInstance().getFrame().pack();
+        //            }
+        //        });
 
     }
 }
