1e90115966760af7bb3b6afb188a04b6d8e6b136
lanlan2017 <18251956727@163.com>
2024-10-29 00:02:31 +0800

    perf(批处理):等待关机.bat可以选择要等待的时间

--- runable/等待关机.bat
+++ runable/等待关机.bat
@@ -1,5 +1,30 @@
+@echo off
 @REM 等待5小时=5*60*60=18000s
-timeout 18000
+@REM timeout 18000
+echo Please select a waiting time:
+echo 1 : 1 hour
+echo 2 : 2 hours
+echo 3 : 3 hours
+echo 4 : 4 hours
+echo 5 : 5 hours
+set /p choice="Please enter your choice(1,2,3,4,5): "
+:: 根据用户输入设置等待时间
+if "%choice%"=="1" (
+    set waitTime=3600
+) else if "%choice%"=="2" (
+    set waitTime=7200
+) else if "%choice%"=="3" (
+    set waitTime=10800
+) else if "%choice%"=="4" (
+    set waitTime=14400
+) else if "%choice%"=="5" (
+      set waitTime=18000
+) else (
+    echo 输入无效,默认等待3小时。
+    set waitTime=10800
+)
+@REM timeout %waitTime%
+timeout /t %waitTime% /nobreak
 @REM 调用另一个批处理文件
 @REM 重置手机电池状态
 call adbbr.bat
